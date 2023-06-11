cbb54a14366a5726bb45ff89c446a3c0a4eff759
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2023-06-11 11:48:55 -0300

    test(page): Add tests for Page class - Create a page instance successfully - Retrieve the page id

---
+++ tests/page.spec.ts
@@ -0,0 +1,26 @@
+import { faker } from "@faker-js/faker"
+
+import Page from "@src/page"
+
+//* yarn test:watch ./tests/page.spec.ts
+
+describe('Page expectations', () => {
+  it('should be able to create a page', () => {
+    const id = faker.string.alpha({ casing: "upper" })
+
+    const page = new Page(id)
+
+    expect(page).toBeTruthy()
+    expect(page).toBeInstanceOf(Page)
+  })
+
+  it('should be able to get page id', () => {
+    const id = faker.string.alpha({ casing: "upper" })
+
+    const page = new Page(id)
+
+    expect(page.id).toBeTruthy()
+    expect(page.id).toBe(id)
+    expect(typeof page.id).toBe("string")
+  })
+})
